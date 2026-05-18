6a6729863f5c8d1d5e673ca9b8958cc19325e5f0
Jonathan Ong <ongaikngee@gmail.com>
2026-05-18 12:11:18 +0800

    feat: set up routing with React Router and add Home, About, and Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
 import Navbar from './components/NavBar'
 
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+
 function App() {
 
   return (
-    <Navbar />
+    <>
+      <Navbar />
+
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
