638cf249b8a841b3f17401d517fc48ed45d01942
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-02 17:42:11 +0800

    Refactor StudPoker component to manage game state and utilize centralized deck back image

--- src/pages/StudPoker.jsx
+++ src/pages/StudPoker.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState } from "react";
 import { getNewDeck, drawCardFromDeck } from "../services/deckService";
+import { IMG_DECK_BACK } from "../constants/games";
 
 const StudPoker = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isActive, setIsActive] = useState(false);
+  const [gameState, setGameState] = useState("idle");
+  //   idle, loading, playerMove, strength
   const [deck, setDeck] = useState(null);
 
   const [playerHand, setPlayerHand] = useState([]);
@@ -33,7 +34,7 @@ const StudPoker = () => {
   };
 
   const startGame = async () => {
-    setIsLoading(true);
+    setGameState("loading");
     try {
       setPlayerHand([]);
       setDealerHand([]);
@@ -43,50 +44,142 @@ const StudPoker = () => {
       if (!fetchDeck) return;
 
       setDeck(fetchDeck);
-      setIsActive(true);
+      setGameState("playerMove");
 
       const playerHand = await getHand(fetchDeck.deck_id);
-      setPlayerHand(playerHand);
+      setPlayerHand(playerHand.cards);
 
       const dealerHand = await getHand(fetchDeck.deck_id);
-      setDealerHand(dealerHand);
+      setDealerHand(dealerHand.cards);
     } catch (e) {
       console.error(e);
-    } finally {
-      setIsLoading(false);
     }
   };
 
-  return (
-    <div className="container my-4">
-      <h2>Stud Poker</h2>
-      {!isActive ? (
+  const bet = () => {
+    setGameState("strength");
+  };
+
+  const fold = () => {
+    setGameState("idle");
+  };
+
+  if (gameState === "idle") {
+    return (
+      <div className="container my-4">
+        <h2>Stud Poker</h2>
         <div className="container">
           <button type="button" className="btn btn-primary" onClick={startGame}>
             Start Game
           </button>
         </div>
-      ) : (
-        <div className="container">
-          {isLoading ? (
-            <div className="container">
-              <div className="spinner-grow" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </div>
-            </div>
-          ) : (
-            <div className="container">
-              <h2>Start</h2>
+      </div>
+    );
+  }
 
-              <p>Deck: {deck?.deck_id}</p>
-              <p>Player: {JSON.stringify(playerHand)}</p>
-              <p>Dealer: {JSON.stringify(dealerHand)}</p>
-            </div>
+  if (gameState === "loading") {
+    return (
+      <div className="container my-4">
+        <h2>Stud Poker</h2>
+        <div className="spinner-grow" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (gameState === "playerMove") {
+    return (
+      <div className="container my-4">
+        <h2>Stud Poker</h2>
+        <p>Deck: {deck?.deck_id}</p>
+        <div className="container">
+          <h2>Dealer's Hand</h2>
+          {dealerHand.map((card, index) =>
+            index === 4 ? (
+              <img
+                key={card.code || index}
+                src={card.image}
+                className="img-fluid"
+                style={{ maxHeight: "100px" }}
+              />
+            ) : (
+              <img
+                key={card.code || index}
+                src={IMG_DECK_BACK}
+                className="img-fluid"
+                style={{ maxHeight: "100px" }}
+              />
+            ),
           )}
         </div>
-      )}
-    </div>
-  );
+
+        <div className="container">
+          <h2>Player's Hand</h2>
+          {playerHand.map((card) => (
+            <img
+              key={card.code || index}
+              src={card.image}
+              className="img-fluid"
+              style={{ maxHeight: "150px" }}
+            />
+          ))}
+        </div>
+        <div className="container">
+          <h2>Action</h2>
+          <span>
+            <button type="button" className="btn btn-success" onClick={bet}>
+              Ante
+            </button>
+            <button type="button" className="btn btn-danger" onClick={fold}>
+              fold
+            </button>
+          </span>
+        </div>
+      </div>
+    );
+  }
+
+  if (gameState === "strength") {
+    return (
+      <div className="container my-4">
+        <h2>Stud Poker</h2>
+        <p>Deck: {deck?.deck_id}</p>
+        <div className="container">
+          <h2>Dealer's Hand</h2>
+          {dealerHand.map((card) => (
+            <img
+              key={card.code}
+              src={card.image}
+              className="img-fluid"
+              style={{ maxHeight: "100px" }}
+            />
+          ))}
+        </div>
+
+        <div className="container">
+          <h2>Player's Hand</h2>
+          {playerHand.map((card) => (
+            <img
+              key={card.code}
+              src={card.image}
+              className="img-fluid"
+              style={{ maxHeight: "150px" }}
+            />
+          ))}
+        </div>
+        <div className="container">
+          <h2>Result</h2>
+
+          <button type="button" className="btn btn-primary" onClick={startGame}>
+            Start Game
+          </button>
+        </div>
+        {/* <p>Player: {JSON.stringify(playerHand)}</p>
+        <p>Dealer: {JSON.stringify(dealerHand)}</p> */}
+      </div>
+    );
+  }
 };
 
 export default StudPoker;
