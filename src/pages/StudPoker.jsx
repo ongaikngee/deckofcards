ba5ff2bf09fc4149fc99c563ce6b70533d7521ff
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-02 16:47:56 +0800

    Refactor StudPoker component to implement asynchronous deck and hand retrieval, enhancing game start functionality

--- src/pages/StudPoker.jsx
+++ src/pages/StudPoker.jsx
@@ -1,19 +1,60 @@
 import React from "react";
 import { useState } from "react";
+import { getNewDeck, drawCardFromDeck } from "../services/deckService";
 
 const StudPoker = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isActive, setIsActive] = useState(false);
-  const [deck, setDeck] = useState();
+  const [deck, setDeck] = useState(null);
 
-  const startGame = () => {
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+
+  const getDeck = async () => {
+    try {
+      const deckData = await getNewDeck({
+        noOfDecks: 1,
+        jokersEnabled: false,
+      });
+      return deckData;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getHand = async (deck_id) => {
+    try {
+      const drawnCards = await drawCardFromDeck(deck_id, 5);
+      return drawnCards;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  };
+
+  const startGame = async () => {
     setIsLoading(true);
-    console.log("Getting Deck");
-    setIsActive(true);
+    try {
+      setPlayerHand([]);
+      setDealerHand([]);
+      setDeck(null);
 
-    setTimeout(() => {
+      const fetchDeck = await getDeck();
+      if (!fetchDeck) return;
+
+      setDeck(fetchDeck);
+      setIsActive(true);
+
+      const playerHand = await getHand(fetchDeck.deck_id);
+      setPlayerHand(playerHand);
+
+      const dealerHand = await getHand(fetchDeck.deck_id);
+      setDealerHand(dealerHand);
+    } catch (e) {
+      console.error(e);
+    } finally {
       setIsLoading(false);
-    }, 5000);
+    }
   };
 
   return (
@@ -29,13 +70,17 @@ const StudPoker = () => {
         <div className="container">
           {isLoading ? (
             <div className="container">
-              <div class="spinner-grow" role="status">
+              <div className="spinner-grow" role="status">
                 <span className="visually-hidden">Loading...</span>
               </div>
             </div>
           ) : (
             <div className="container">
               <h2>Start</h2>
+
+              <p>Deck: {deck?.deck_id}</p>
+              <p>Player: {JSON.stringify(playerHand)}</p>
+              <p>Dealer: {JSON.stringify(dealerHand)}</p>
             </div>
           )}
         </div>
