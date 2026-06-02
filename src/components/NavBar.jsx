c447f77b8c15fd40b6a7da37c88b34cfc91ae127
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-02 15:04:51 +0800

    Add Stud Poker page and update navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,16 +22,21 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
+              <Link className="nav-link" to="/studpoker">
+                Stud Poker
               </Link>
             </li>
-
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Games
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+
 
             <li className="nav-item">
               <Link className="nav-link" to="/contact">
---
+++ src/pages/StudPoker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StudPoker = () => {
+  return (
+    <div className="container my-4">
+      <h2>Stud Poker</h2>
+    </div>
+  );
+};
+
+export default StudPoker;
