2538fbd6d924ef030285096d2b17f148ebc20044
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-04 20:47:16 +0800

    Feature: determine if dealer qualified during showdown.

--- src/pages/StudPoker.jsx
+++ src/pages/StudPoker.jsx
@@ -12,14 +12,14 @@ const StudPoker = () => {
   const [deck, setDeck] = useState(null);
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
-  const [playerStrength, setPlayerStrength] = useState("");
-  const [dealerStrength, setDealerStrength] = useState("");
+  const [playerStrength, setPlayerStrength] = useState(null);
+  const [dealerStrength, setDealerStrength] = useState(null);
   const [winner, setWinner] = useState("");
 
   const [chips, setChips] = useState(1000);
   const [betAmount, setBetAmount] = useState(50);
 
- 
+  const [isDealerQualified, setIsDealerQualified] = useState(false);
 
   const getDeck = async () => {
     try {
@@ -67,22 +67,48 @@ const StudPoker = () => {
     }
   };
 
+  // Dealer needs to have at least a Ace King to qualify. 
+  // If the dealer does not qualify, the player wins and is paid 1:1 on the ante bet, 
+  // and the play bet is returned to the player. 
+  // If the dealer qualifies, then the hands are compared to determine the winner. 
+  // If the dealer's hand is better than the player's hand, the dealer wins and takes both the 
+  // ante and play bets. 
+  // If the player's hand is better than the dealer's hand, the player wins and is paid 1:1 on both the 
+  // ante and play bets.
+  const checkDealerQualification = (strength) => {
+    if (strength.rank > 1) {
+      return true;
+    }
+
+    if (strength.descr === "A High") {
+      return strength.values.some((slot) =>
+        slot?.some((card) => card.value === "K"),
+      );
+    }
+
+    return false;
+  };
+
   const getStrengthOfHand = (hands) => {
     const codes = hands.map((card) => card.code.replace("0", "T"));
     const hand = Hand.solve(codes);
-    return hand.descr;
+    return hand;
   };
 
   useEffect(() => {
     if (gameState === "playerMove" && playerHand.length === 5) {
       const strength = getStrengthOfHand(playerHand);
-      setPlayerStrength(strength);
+      setPlayerStrength(strength.descr);
     }
     if (gameState === "playerMove" && dealerHand.length === 5) {
       const strength = getStrengthOfHand(dealerHand);
-      setDealerStrength(strength);
+      setDealerStrength(strength.descr);
+      const dealerQ = checkDealerQualification(strength);
+      console.log("did dealer qualify?", dealerQ);
+      setIsDealerQualified(dealerQ);
     }
     if (gameState === "playerBet") {
+      // TODO: Do you need to call the functions 3 times?
       setWinner(getWinner());
       if (getWinner() === "Player") {
         setChips((prev) => prev + betAmount * 4);
@@ -101,8 +127,8 @@ const StudPoker = () => {
       // reset all state for new game
       setPlayerHand([]);
       setDealerHand([]);
-      setPlayerStrength("");
-      setDealerStrength("");
+      setPlayerStrength(null);
+      setDealerStrength(null);
       setWinner("");
       setDeck(null);
 
@@ -261,6 +287,7 @@ const StudPoker = () => {
               <CheckIcon size={32} weight="bold" className="text-success" />
             )}
           </div>
+          {isDealerQualified ? <p>Dealer qualified</p> : <p>Dealer did not qualified</p>}
           <DisplayCards cards={dealerHand} size={100} />
           <h3>{dealerStrength}</h3>
         </div>
