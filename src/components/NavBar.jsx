fb7744def9fac5afb1815f058535625455b17d34
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2026-06-08 14:18:41 +0800

    Feature: Authenticated User access to links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../features/auth/AuthContext";
+
 import Collapse from "bootstrap/js/dist/collapse"
 
 import { PersonArmsSpreadIcon } from "@phosphor-icons/react";
 
 function Navbar() {
+  const { user } = useAuth();
+
   const closeNavbar = () => {
     const nav = document.getElementById("navbarNav");
     if (nav) {
@@ -30,33 +34,40 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/studpoker" onClick={closeNavbar}>
-                Stud Poker
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={closeNavbar}>
-                Games
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about" onClick={closeNavbar}>
-                About
-              </Link>
-            </li>
+            {!user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about" onClick={closeNavbar}>
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact" onClick={closeNavbar}>
+                    Contact
+                  </Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/studpoker" onClick={closeNavbar}>
+                    Stud Poker
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/" onClick={closeNavbar}>
+                    Games
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/user" onClick={closeNavbar}>
+                    <PersonArmsSpreadIcon size={24} />
+                  </Link>
+                </li>
+              </>
+            )}
 
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact" onClick={closeNavbar}>
-                Contact
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/user" onClick={closeNavbar}>
-                <PersonArmsSpreadIcon size={24} />
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
