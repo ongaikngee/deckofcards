26eea2849f3712c1a314e96a7770573b4dca922b
Ong Aik Ngee <73453714+ongaikngee@users.noreply.github.com>
2026-06-09 17:20:06 +0800

    Fix: unique "key" prop issue in StudPokerHistory

--- src/features/games/StudPokerHistory.jsx
+++ src/features/games/StudPokerHistory.jsx
@@ -31,52 +31,51 @@ export const StudPokerHistory = ({ SPGames }) => {
 				</thead>
 				<tbody>
 					{SPGames.map((game, index) => (
-						<>
-							<tr key={index}
-								className={`container ${game.winner === GAME_RESULT.WINNER_PLAYER ? "table-primary" : "table-danger"}`}
-							>
-								<td>
-									<div>
-										<DisplayCards cards={game.playerHand} size={80} />
-										<div className="d-flex align-items-center gap-2 p-1">
-											{game.winner === GAME_RESULT.WINNER_PLAYER && (
-												<CheckIcon size={20} weight="bold" className="text-success" />
-											)}
-											<div>{game.playerStrength.descr}</div>
-											{game.playerAction === PLAYER_ACTION.FOLD &&
-												<span className="badge text-bg-danger">Fold</span>}
-										</div>
+						<tr key={index}
+							className={`container ${game.winner === GAME_RESULT.WINNER_PLAYER ? "table-primary" : "table-danger"}`}
+						>
+							<td>
+								<div>
+									<DisplayCards cards={game.playerHand} size={80} />
+									<div className="d-flex align-items-center gap-2 p-1">
+										{game.winner === GAME_RESULT.WINNER_PLAYER && (
+											<CheckIcon size={20} weight="bold" className="text-success" />
+										)}
+										<div>{game.playerStrength.descr}</div>
+										{game.playerAction === PLAYER_ACTION.FOLD &&
+											<span className="badge text-bg-danger">Fold</span>}
+									</div>
 
-										<div className="d-flex align-items-center gap-2">
-											{game.payoutAmt !== 0 && (
-												<span
-													className={`badge ${game.payoutAmt > 0 ? 'text-bg-success' : 'text-bg-danger'
-														}`}
-												>
-													{formatCurrency(game.payoutAmt)}
-												</span>
-											)}
-											{game.winningMultiplier && (
-												<span className="badge text-bg-warning">{game.winningMultiplier}x - {game.winningPokerHandClass}</span>
-											)}
-										</div>
+									<div className="d-flex align-items-center gap-2">
+										{game.payoutAmt !== 0 && (
+											<span
+												className={`badge ${game.payoutAmt > 0 ? 'text-bg-success' : 'text-bg-danger'
+													}`}
+											>
+												{formatCurrency(game.payoutAmt)}
+											</span>
+										)}
+										{game.winningMultiplier && (
+											<span className="badge text-bg-warning">{game.winningMultiplier}x - {game.winningPokerHandClass}</span>
+										)}
 									</div>
-								</td>
-								<td>
-									<div>
-										<DisplayCards cards={game.dealerHand} size={80} />
-										<div className="d-flex align-items-center gap-2 p-1">
-											{game.winner === GAME_RESULT.WINNER_DEALER && (
-												<CheckIcon size={20} weight="bold" className="text-success" />
-											)}
-											<div>{game.dealerStrength.descr}</div>
-											{game.playerAction === "Did not qualified" &&
-												<span className="badge text-bg-danger">Did not qualified</span>}
-										</div>
+								</div>
+							</td>
+							<td>
+								<div>
+									<DisplayCards cards={game.dealerHand} size={80} />
+									<div className="d-flex align-items-center gap-2 p-1">
+										{game.winner === GAME_RESULT.WINNER_DEALER && (
+											<CheckIcon size={20} weight="bold" className="text-success" />
+										)}
+										<div>{game.dealerStrength.descr}</div>
+										{game.playerAction === "Did not qualified" &&
+											<span className="badge text-bg-danger">Did not qualified</span>}
 									</div>
-								</td>
-							</tr>
-						</>
+								</div>
+							</td>
+						</tr>
+
 					))}
 				</tbody>
 			</table>
