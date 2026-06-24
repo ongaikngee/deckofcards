20e2019c4aefe1a5a0c38fb1dd8ff2b315f31b3f
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-24 16:08:54 +0800

    Feature: User can top up chips

--- src/features/users/Chips.jsx
+++ src/features/users/Chips.jsx
@@ -1,14 +1,92 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { PokerChipIcon } from "@phosphor-icons/react";
+import {
+  getChipHistoryService,
+  updateChipAmtService,
+} from "../../services/chipService";
+
+import { useAuth } from "../auth/AuthContext";
+
+// helpers
+import { formatCurrency } from "../../utils/formatCurrency";
 
 const Chips = () => {
+  const [chips, setChips] = useState([]);
+  const [error, setError] = useState("");
+  const [topUpAmt, setTopUpAmt] = useState(0);
+
+  const { user } = useAuth();
+
+  const getChipHistory = async () => {
+    setError("");
+    try {
+      const response = await getChipHistoryService(user);
+      setChips(response);
+    } catch (e) {
+      setError(e);
+      console.log(e);
+    }
+  };
+
+  const topUpChip = async () => {
+    setError("");
+    try {
+      const response = await updateChipAmtService(user, topUpAmt);
+      setChips(response);
+    } catch (e) {
+      setError(e);
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getChipHistory();
+  }, []);
+
   return (
     <div>
       <h2>
         <PokerChipIcon size={44} />
         Chips
       </h2>
+      {chips && (
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">Amount</th>
+              <th scope="col">Reason</th>
+            </tr>
+          </thead>
+          <tbody>
+            {chips.map((record) => (
+              <tr key={record.id}>
+                <td>{record.id}</td>
+                <td>{formatCurrency(record.amount)}</td>
+                <td>{record.reason}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <hr></hr>
+      <div className="h2">Top Up</div>
+      {/* Betting Amount Range Selector */}
+      <div className="col-sm-6 mt-3">
+        <input
+          type="range"
+          className="form-range"
+          id="betSize"
+          min="0"
+          max="100000"
+          step="1000"
+          value={topUpAmt}
+          onChange={(e) => setTopUpAmt(e.target.valueAsNumber)}
+        ></input>
+      </div>
+      <button className="btn btn-primary" onClick={() => topUpChip()}>
+        Top up {formatCurrency(topUpAmt)}
+      </button>
     </div>
   );
 };
---
+++ src/services/chipService.js
@@ -0,0 +1,65 @@
+const API_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = `${API_URL}/chips`;
+
+export const getChipHistoryService = async (user_id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${user_id}`);
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching chips data:", error);
+    throw error;
+  }
+};
+
+export const updateChipAmtService = async (user_id, amt, reason = "Topup") => {
+  const response = await fetch(`${BASE_URL}/${user_id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount: amt, reason: reason }),
+  });
+
+  let data;
+  try {
+    data = await response.json();
+  } catch (jsonError) {
+    data = null;
+  }
+
+  if (!response.ok) {
+    const errorMessage = data?.detail || data?.message || "Top up failed";
+    const error = new Error(errorMessage);
+    error.status = response.status;
+    error.payload = data;
+    throw error;
+  }
+};
+
+// export const registerUser = async (username, password) => {
+//   const response = await fetch(`${API_URL}/users/`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ username, password }),
+//   });
+
+//   let data;
+//   try {
+//     data = await response.json();
+//   } catch (jsonError) {
+//     data = null;
+//   }
+
+//   if (!response.ok) {
+//     const errorMessage = data?.detail || data?.message || "Registration failed";
+//     const error = new Error(errorMessage);
+//     error.status = response.status;
+//     error.payload = data;
+//     throw error;
+//   }
+
+//   return data;
+// };
