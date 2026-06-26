dafce999c9ef5a1a3e7858fd95a7d4d383647fd5
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-06-26 10:47:41 +0800

    Refactor: Arrange links

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { useState } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
-import ProtectedRoute from "./features/auth/ProtectedRoute";
-
 import Games from "./pages/Games";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -14,10 +12,12 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import StudPoker from "./pages/StudPoker";
 
+import ProtectedRoute from "./features/auth/ProtectedRoute";
+import AdminRoute from "./features/auth/AdminRoute";
+
 import UserMain from "./features/users/UserMain";
 import Settings from "./features/users/Settings";
 import Chips from "./features/users/Chips";
-import AdminRoute from "./features/auth/AdminRoute";
 import AdminPage from "./pages/AdminPage";
 
 import { CurrentGame } from "./features/games/CurrentGame";
@@ -43,10 +43,7 @@ function App() {
             <Route path="/user/chips" element={<ProtectedRoute><Chips /></ProtectedRoute>} />
             <Route path="/user/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
           </Route>
-          <Route
-            path="/game/:deckId"
-            element={<CurrentGame games={games} setGames={setGames} />}
-          />
+          <Route path="/game/:deckId" element={<CurrentGame games={games} setGames={setGames} />} />
           <Route path="/studpoker" element={<ProtectedRoute><StudPoker /></ProtectedRoute>} />
           <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
         </Routes>
